8c967424bdf6a0f68745c20724bce6e002f16ef6
Karina Hristenko <karinahristenko@gmail.com>
2022-11-30 21:06:28 +0200

    [master] Add classroom progress 30.11

---
+++ src/main/java/lv/acodemy/step_definitions/ProductsPageSteps.java
@@ -0,0 +1,17 @@
+package lv.acodemy.step_definitions;
+
+import io.cucumber.java.en.Given;
+import lv.acodemy.page_object.ProductsPage;
+
+public class ProductsPageSteps {
+
+    private final ProductsPage productsPage = new ProductsPage();
+
+    @Given("user selects {string} product from a list")
+    public void userSelectsProductFromList(String productName) {
+        productsPage.clickOnProductByName(productName);
+
+
+
+    }
+}
